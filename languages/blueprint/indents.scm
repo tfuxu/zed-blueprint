3f6808893f601c23dad6d2a745fcbf76ac2261ad
tfuxu <73042332+tfuxu@users.noreply.github.com>
2026-09-04 22:38:19 +0200

    fix: double indentation caused by a duplicate indent list - format indents rules file

--- languages/blueprint/indents.scm
+++ languages/blueprint/indents.scm
@@ -1,30 +1,6 @@
-[
-  (object_content)
-  (menu)
-  (menu_submenu)
-  (menu_section)
-  (menu_item)
-  (menu_item_shorthand)
-  (try_expression)
-  (ext_accessibility)
-  (ext_adw_dialog)
-  (ext_adw_breakpoint)
-  (ext_adw_breakpoint_condition)
-  (ext_combo_box_items)
-  (ext_file_filter_mime_types)
-  (ext_file_filter_patterns)
-  (ext_file_filter_suffixes)
-  (ext_layout)
-  (ext_level_bar_offsets)
-  (ext_scale_marks)
-  (ext_scale_mark)
-  (ext_size_group_widgets)
-  (ext_string_list_strings)
-  (ext_styles)
-] @indent
-
 (object_content
   "}" @end) @indent
+
 (menu
   "}" @end) @indent
 (menu_submenu
@@ -35,35 +11,47 @@
   "}" @end) @indent
 (menu_item_shorthand
   ")" @end) @indent
+
 (try_expression
   "}" @end) @indent
+
 (ext_accessibility
   "}" @end) @indent
+
 (ext_adw_dialog
   "]" @end) @indent
+
 (ext_adw_breakpoint
   "}" @end) @indent
 (ext_adw_breakpoint_condition
   ")" @end) @indent
+
 (ext_combo_box_items
   "]" @end) @indent
+
 (ext_file_filter_mime_types
   "]" @end) @indent
 (ext_file_filter_patterns
   "]" @end) @indent
 (ext_file_filter_suffixes
   "]" @end) @indent
+
 (ext_layout
   "}" @end) @indent
+
 (ext_level_bar_offsets
   "]" @end) @indent
+
 (ext_scale_marks
   "]" @end) @indent
 (ext_scale_mark
   ")" @end) @indent
+
 (ext_size_group_widgets
   "]" @end) @indent
+
 (ext_string_list_strings
   "]" @end) @indent
+
 (ext_styles
   "]" @end) @indent
